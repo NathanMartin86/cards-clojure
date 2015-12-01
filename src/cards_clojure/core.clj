3bdcfd2c5363641629df662add20c70b6c7b1378
macbookair <nj.martin123@gmail.com>
2015-11-30 20:00:42 -0500

    9.1

--- src/cards_clojure/core.clj
+++ src/cards_clojure/core.clj
@@ -3,13 +3,13 @@
 
 (def suits [:clubs :spades :hearts :diamonds])
 (def ranks (range 1 14))
-(def rank-names {1 :ace, 11 :Jack, 12 :Queen, 13 :King})
+;(def rank-names {1 :ace, 11 :Jack, 12 :Queen, 13 :King})
 
 (defn create-deck []
   (set (for [suit suits
              rank ranks]
          {:suit suit
-          :rank (get rank-names rank rank)})))
+          :rank rank})))
 
 (defn create-hands [deck]
   (set (for [c1 deck
@@ -21,10 +21,30 @@
 (defn flush? [hand]
   (= 1 (count (set (map :suit hand)))))
 
+(defn most-popular-numbers
+  [hand] (sort > (map second (frequencies (map second hand)))))
+
+(defn straight? [hand]
+  (let [[min-value :as sorted] (sort (map :rank hand))]
+    (= sorted (take 4 (iterate inc min-value)))))
+
+(defn four-of-a-kind? [hand]
+  (= 1 (count (set (map :rank hand)))))
+
+(defn three-of-a-kind? [hand]
+  (== (apply max (vals (frequencies (map :rank hand))))3))
+
+(defn two-pair? [hand]
+  (= '(2 2) (take 2 (most-popular-numbers hand))))
+
+(defn straight-flush? [hand]
+  (and (straight? hand) (flush? hand)))
+
 
 (defn -main
   [& args]
+
   (time (let [deck (create-deck)
-              hands (create-hands deck)
-              hands (filter flush? hands)]
-          (println (count hands)))))
+                hands (create-hands deck)
+                hands (filter flush? hands)]
+            (println (count hands)))))
--- test/cards_clojure/core_test.clj
+++ test/cards_clojure/core_test.clj
@@ -12,15 +12,58 @@
                :rank 5}})
 
 (def hand-2 #{{:suit :diamonds
-              :rank 2}
-             {:suit :clubs
-              :rank 3}
-             {:suit :clubs
-              :rank 4}
-             {:suit :clubs
-              :rank 5}})
+               :rank 2}
+              {:suit :clubs
+               :rank 3}
+              {:suit :clubs
+               :rank 4}
+              {:suit :clubs
+               :rank 5}})
+
+(def hand-3 #{{:suit :diamonds
+               :rank 2}
+              {:suit :clubs
+               :rank 2}
+              {:suit :clubs
+               :rank 4}
+              {:suit :diamonds
+               :rank 4}})
+
+(def hand-4 #{{:suit :diamonds
+               :rank 2}
+              {:suit :clubs
+               :rank 2}
+              {:suit :spades
+               :rank 2}
+              {:suit :diamonds
+               :rank 4}})
+
+(def hand-5  #{{:suit :diamonds
+               :rank 10}
+              {:suit :diamonds
+               :rank 9}
+              {:suit :diamonds
+               :rank 8}
+              {:suit :diamonds
+               :rank 7}})
+(def hand-6  #{{:suit :diamonds
+                :rank 4}
+               {:suit :clubs
+                :rank 4}
+               {:suit :spades
+                :rank 4}
+               {:suit :hearts
+                :rank 4}})
+
 
 (deftest flush-test
-  (testing "flush returns true if the hand is a flush"
+  (testing "testing all the hand variations to see if they work"
     (is (= true (flush? hand-1)))
-    (is (= false (flush? hand-2)))))
+    (is (= false (flush? hand-2)))
+    (is (= true (straight? hand-2)))
+    (is (= true (two-pair? hand-3)))
+    (is (= true (three-of-a-kind? hand-4)))
+    (is (= true (straight-flush? hand-5)))
+    (is (= true (four-of-a-kind? hand-6)))))
+
+
